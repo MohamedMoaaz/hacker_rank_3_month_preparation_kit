678cc8bf95250b19d043c244bc7b6376a553c884
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-02 11:28:38 +0000

    solve pb2 week 1

---
+++ week1.py
@@ -0,0 +1,20 @@
+
+#Plus Minus
+
+def plusMinus(arr):
+    arrSize = len(arr)
+    countPositive = sum(1 for x in arr if x > 0)
+    countNegative = sum(1 for x in arr if x < 0)
+    countZeros = arrSize - countPositive - countNegative
+    
+    print(f"{countPositive/arrSize:.6f}")
+    print(f"{countNegative/arrSize:.6f}")
+    print(f"{countZeros/arrSize:.6f}")
+
+# Mini Max Sum
+
+def miniMaxSum(arr):
+    arr.sort()
+    miniSum = sum(arr[:4])
+    maxSum = sum(arr[1:5])
+    print(f"{miniSum} {maxSum}")
