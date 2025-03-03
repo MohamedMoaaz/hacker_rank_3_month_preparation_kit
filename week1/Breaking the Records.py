7f5a20cdf0dd778866334d7d3a67d51d2f3a0e90
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-03 12:42:10 +0200

    Create Breaking the Records.py

---
+++ week1/Breaking the Records.py
@@ -0,0 +1,16 @@
+# Breaking the Records
+
+def breakingRecords(scores):
+    _min = scores[0]
+    _max = scores[0]
+    cmin = 0
+    cmax = 0
+    for i in scores:
+        if i < _min:
+            _min = i
+            cmin += 1
+        elif i > _max:
+            _max = i
+            cmax +=1
+    new_arr = [cmax, cmin]
+    return new_arr
