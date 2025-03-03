1ce56026227f56a0af6cb31e498c3d374a0ab051
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-03 12:37:00 +0200

    Update and rename Plus Minus to Plus-Minus.py

---
+++ week1/Plus Minus.py
@@ -0,0 +1,13 @@
+
+#Plus Minus
+
+def plusMinus(arr):
+  arrSize = len(arr)
+  countPositive = sum(1 for x in arr if x > 0)
+  countNegative = sum(1 for x in arr if x < 0)
+  countZeros = arrSize - countPositive - countNegative
+  
+  print(f"{countPositive/arrSize:.6f}")
+  print(f"{countNegative/arrSize:.6f}")
+  print(f"{countZeros/arrSize:.6f}")
+
