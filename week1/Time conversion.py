94e59b5a807dd8ab18bc400ed37d7a8f30f35ba2
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-03 12:40:48 +0200

    Create Time conversion.py

---
+++ week1/Time conversion.py
@@ -0,0 +1,15 @@
+# Time Conversion
+
+def timeConversion(s):
+    timeSplit = s.split(':')
+    hours = timeSplit[0]
+    mins = timeSplit[1]
+    secs = timeSplit[2][:-2]
+    if 'AM' in s:
+        if hours == '12':
+            hours = '00'
+    else:
+        if '12' not in hours:
+            hours = str(int(hours) + 12)
+            
+    return (hours + ':' + mins + ':' + secs)
