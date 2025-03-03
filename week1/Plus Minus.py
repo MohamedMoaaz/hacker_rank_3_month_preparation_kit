5a48aec711ff0232c62da7a391a8aae7a479e13c
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-03 12:39:01 +0200

    Update Plus Minus.py

--- week1/Plus Minus.py
+++ week1/Plus Minus.py
@@ -2,12 +2,12 @@
 #Plus Minus
 
 def plusMinus(arr):
-  arrSize = len(arr)
-  countPositive = sum(1 for x in arr if x > 0)
-  countNegative = sum(1 for x in arr if x < 0)
-  countZeros = arrSize - countPositive - countNegative
-  
-  print(f"{countPositive/arrSize:.6f}")
-  print(f"{countNegative/arrSize:.6f}")
-  print(f"{countZeros/arrSize:.6f}")
+    arrSize = len(arr)
+    countPositive = sum(1 for x in arr if x > 0)
+    countNegative = sum(1 for x in arr if x < 0)
+    countZeros = arrSize - countPositive - countNegative
+    
+    print(f"{countPositive/arrSize:.6f}")
+    print(f"{countNegative/arrSize:.6f}")
+    print(f"{countZeros/arrSize:.6f}")
 
