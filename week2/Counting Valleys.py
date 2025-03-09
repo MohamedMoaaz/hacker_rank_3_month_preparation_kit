d94528169293fd7ef53028c8eb98841de3262cc5
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-09 23:06:23 +0200

    Create Counting Valleys.py

---
+++ week2/Counting Valleys.py
@@ -0,0 +1,11 @@
+def countingValleys(steps, path):
+    level = 0
+    vallies = 0
+    for step in path:
+        if step == 'U':
+            level += 1
+            if level == 0:
+                vallies +=1
+        elif step == 'D':
+            level -= 1
+    return vallies
