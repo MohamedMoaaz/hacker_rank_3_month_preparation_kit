5b40809a1512b0c499a9d685ec8df353de37a0c0
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-12 03:02:16 +0200

    Create Sparse Arrays.py

---
+++ week1/Sparse Arrays.py
@@ -0,0 +1,13 @@
+def matchingStrings(strings, queries):
+    string_counts = {}
+    for string in strings:
+        if string in string_counts:
+            string_counts[string] += 1
+        else:
+            string_counts[string] = 1
+    
+    result = []
+    for query in queries:
+        result.append(string_counts.get(query, 0))
+    
+    return result
