776dab6169e76b87917dd08844997b7a91e52785
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-22 00:12:37 +0200

    Create Sales by Match.py

---
+++ week3/Sales by Match.py
@@ -0,0 +1,12 @@
+def sockMerchant(n, ar):
+    pairs = 0
+    socks_dict = {}
+    for sock in ar:
+        if sock in socks_dict:
+            socks_dict[sock] +=1
+        else :
+            socks_dict[sock] =1
+             
+    for value in socks_dict.values():
+        pairs += value // 2
+    return pairs
