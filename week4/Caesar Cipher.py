e256f24926ed4090158324dc87cbdf4cea3ce55c
Mohamed Moaaz <Mohamedmoaaz646@gmail.com>
2025-03-27 16:40:53 +0200

    Update Caesar Cipher.py

--- week4/Caesar Cipher.py
+++ week4/Caesar Cipher.py
@@ -1,1 +1,12 @@
-
+def caesarCipher(s, k):
+    result = ""
+    for string in s:
+        if string.isalpha():
+            ascii = ord(string)
+            if string.isupper():
+                result += chr(65 + ((ascii + k - 65) % 26))
+            else:
+                result += chr(97 + ((ascii + k - 97) % 26))
+        else:
+            result += string
+    return result
